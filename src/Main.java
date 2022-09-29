e46cfc2d2b0768cc2b9421e41c04642d950e4ecb
EvgeniyPerm <112462287+EvgeniyPerm@users.noreply.github.com>
2022-09-29 21:44:55 +0500

    Given a sorted integer array arr, two integers k and x, return the k closest integers to x in the array. The result should also be sorted in ascending order.
    An integer a is closer to x than an integer b if:
    
    |a - x| < |b - x|, or
    |a - x| == |b - x| and a < b
    
    Example 1:
    
    Input: arr = [1,2,3,4,5], k = 4, x = 3
    Output: [1,2,3,4]
    Example 2:
    
    Input: arr = [1,2,3,4,5], k = 4, x = -1
    Output: [1,2,3,4]

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Solution solution = new Solution();
+        int[] elements = {-2,-1,1,2,3,4,5};
+//        int[] elements = {1,2,3,4,5,6,7,8};
+        System.out.println("array = "+Arrays.toString(elements));
+        int k = 6;
+        int x = 7;
+        System.out.println("x = " + x);
+        System.out.println("k = " + k);
+        System.out.println("=====");
+        List<Integer> list = solution.findClosestElements(elements,k,x);
+        System.out.println(list.toString());
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Solution {
+import java.util.*;
+
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        if (arr.length==0 || k==0 || k>arr.length) return null;
+        ArrayList<Integer> numbersNeed = new ArrayList<Integer>();
+        int nRight=0;
+        while (nRight< arr.length && arr[nRight]<x){ nRight++; };
+        int nLeft=nRight-1;
+        int cnt=0;
+        while (cnt<k){
+            if (nLeft<0){
+                for (int i = cnt; i < k; i++) {
+                    numbersNeed.add(arr[i]);
+                }
+                Collections.sort(numbersNeed);
+                return numbersNeed;
+            }
+            if (nRight>=arr.length){
+                for (int i = nLeft; i > nLeft-(k-cnt); i--) {
+                    numbersNeed.add(arr[i]);
+                }
+                Collections.sort(numbersNeed);
+                return numbersNeed;
+            }
+            if (Math.abs(arr[nLeft]-x)<=Math.abs(arr[nRight]-x)){
+                numbersNeed.add(arr[nLeft--]);
+            } else{
+                numbersNeed.add(arr[nRight++]);
+            }
+            cnt++;
+        }
+        Collections.sort(numbersNeed);
+        return numbersNeed;
+
+    }
 }
